0bc33bc2649ff25b8b0e2e7d19faa69c05b44ce7
zitian <zitian@mogujie.com>
2015-03-21 18:37:42 +0800

    Initial Commit

---
+++ Redis/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Redis
+//
+//  Created by xiaozi on 3/21/15.
+//  Copyright (c) 2015 xiaozi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Redis/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Redis
+//
+//  Created by xiaozi on 3/21/15.
+//  Copyright (c) 2015 xiaozi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
